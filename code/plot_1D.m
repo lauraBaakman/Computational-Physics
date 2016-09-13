78500f6e91ece1764373ba97be7293a6e147e8a8
Laura Baakman <laurabaakman@gmail.com>
2016-09-13 16:01:02 +0200

    Use real data

--- code/plot_1D.m
+++ code/plot_1D.m
@@ -1,6 +1,5 @@
 clc; close all; clear all; 
-% load('../results/1D.mat');
-load('../results/test1D.mat')
+load('../results/1D.mat');
 
 %% Average Energy for N_samples = 1000, T = 0.2:4, N = 10, 100, 1000
 figure = plots.averageEnergy1D(experiments, 1000);
