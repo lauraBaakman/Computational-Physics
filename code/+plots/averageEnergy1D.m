4bb89ed6c989504b41a403c3ebfcd5a20906d5f9
Laura Baakman <laurabaakman@gmail.com>
2016-09-13 13:23:54 +0200

    Set up of the plots.

---
+++ code/+plots/averageEnergy1D.m
@@ -0,0 +1,13 @@
+function [ plotHandle ] = averageEnergy1D( configurations, numberOfSamples)
+%ONEDAVERAGEENERGY Generate the Plotly handles for the U plots.
+%   Plots U/N as a function of T for different values of N, i.e. number of
+%   samples.
+
+    plotHandle = [];
+    T = 0.2:0.2:4;
+    % Filter configurations for specific value of NSamples
+    % Create subsets for N = 10, 100, 1000
+    % Plot the subsets
+    % Set the label with the used LaTeX symbols in the report.
+end
+
--- code/plot_1D.m
+++ code/plot_1D.m
@@ -1,3 +1,5 @@
 clc; close all; clear all; 
 load('../results/1D.mat')
 
+%% Average Energy for N_samples = 1000, T = 0.2:4, N = 10, 100, 1000
+
