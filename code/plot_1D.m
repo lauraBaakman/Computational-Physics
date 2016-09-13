9e453bf98de24bf0c395f64e83b6a7303238d38a
Laura Baakman <laurabaakman@gmail.com>
2016-09-13 16:19:21 +0200

    Specific heat plots should be generated with specicic heat function.

--- code/plot_1D.m
+++ code/plot_1D.m
@@ -10,9 +10,9 @@ figure = plots.averageEnergy1D(experiments, 10000);
 saveplotlyfig(figure, '../report/img/1D/1DaverageEnergyN10000.pdf');
 
 %% Sepcific Heat for N_samples = 1000, T = 0.2:4, N = 10, 100, 1000
-figure = plots.averageEnergy1D(experiments, 1000);
+figure = plots.specificHeat1D(experiments, 1000);
 saveplotlyfig(figure, '../report/img/1D/1DspecificHeatN1000.pdf');
 
 %% Sepcific Heat for N_samples = 10000, T = 0.2:4, N = 10, 100, 1000
-figure = plots.averageEnergy1D(experiments, 10000);
+figure = plots.specificHeat1D(experiments, 10000);
 saveplotlyfig(figure, '../report/img/1D/1DspecificHeatN10000.pdf');
