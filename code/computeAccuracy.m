d6d172d8333aec76958268c8b2343ebd46fc9263
Laura Baakman <laurabaakman@gmail.com>
2016-09-14 15:42:57 +0200

    Code to generate tables

---
+++ code/computeAccuracy.m
@@ -0,0 +1,6 @@
+function [ accuracy ] = computeAccuracy( actual, theory )
+%ACCURACY Compute the accuracy.
+
+accuracy = ones(size(actual)) - abs((abs(actual - theory) ./ actual));
+
+end
---
+++ code/tables_1D.m
@@ -0,0 +1,4 @@
+clc; close all; clear all; 
+load('../results/small1D.mat');
+
+output.generateLaTeXTable(experiments, 1000, 10);
