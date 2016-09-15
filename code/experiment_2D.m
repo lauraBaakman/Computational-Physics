39fe82e1d75870b4156f9e150b41519b14723a5c
Laura Baakman <laurabaakman@gmail.com>
2016-09-15 15:35:28 +0200

    Set up of experiment.

---
+++ code/experiment_2D.m
@@ -0,0 +1,57 @@
+clc; close all; clear all; 
+rng('default');
+
+computeNumRelaxIterations = @(n) 1/10 .* n;
+
+%% Init 
+% temperatures = 0.2:0.2:4;
+temperatures = 0.2:1:4;
+% dimensionalityList = [10, 50, 100];
+dimensionalityList = [2, 5];
+% numSampleIterationsList = [1000, 10000];
+numSampleIterationsList = [2, 10];
+
+idx = 1;
+
+%% Run Simulations
+for dimensionality = dimensionalityList
+    numParticles = dimensionality * dimensionality;
+    
+    for numSampleIterations =  numSampleIterationsList
+        numRelaxIterations = computeNumRelaxIterations(numSampleIterations);
+        initialConfiguration = ones([dimensionality, dimensionality]);
+        
+        for temperature = temperatures
+          
+          parameters = struct(...
+            'temperature', temperature,...
+            'numParticles', numParticles,...
+            'numSampleIterations', numSampleIterations,...
+            'numRelaxIterations', numRelaxIterations,...
+            'neighborFunction', @neighbors.TwoD4Connected);
+          
+          configurations = MMCIsing(initialConfiguration, parameters);
+
+          % The initial configuration for the next temperature is the
+          % final configuration of the previous temperature
+          initialConfiguration = configurations(:,:,end);
+          
+          parameters = rmfield(parameters, 'neighborFunction');
+          
+          experiments(idx).parameters = parameters;
+          experiments(idx).configurations = configurations;
+          
+          U = computeAverageEnergy(configurations);
+          C = computeSpecificHeat(configurations, parameters.temperature);
+          M = computeMagnetization(configurations);
+          
+          experiments(idx).statistics = struct('averageEnergy', U,...
+              'specificHeat', C,
+              'magnetization', M);
+          idx = idx + 1;
+        end
+    end
+end
+
+%% Store the results
+save('../results/2D.mat', 'experiments')
