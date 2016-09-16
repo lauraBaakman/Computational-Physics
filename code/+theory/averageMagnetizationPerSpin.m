44df29705086877e6afedafef11e80aecb44c3f4
Laura Baakman <laurabaakman@gmail.com>
2016-09-16 13:49:00 +0200

    No need to average here.

--- code/+theory/averageMagnetizationPerSpin.m
+++ code/+theory/averageMagnetizationPerSpin.m
@@ -1,11 +1,10 @@
-function [ average ] = averageMagnetizationPerSpin( temperatures)
+function [ magnetization ] = averageMagnetizationPerSpin( temperatures)
 %AVERAGEMAGNETIZATIONPERSPIN2D Theoretical average energy per spin.
 
 T_c = 2 / log(1 + sqrt(2));
 betas =  1 ./ temperatures;
 
 magnetization = (temperatures < T_c) .* (ones(size(betas)) - sinh(2 .* betas).^(-4)).^2;
-average = mean(magnetization);
 
 end
 
