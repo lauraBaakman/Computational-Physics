736eb4ccf7359782109d31420f75302e759c6921
Laura Baakman <laurabaakman@gmail.com>
2016-10-07 12:17:16 +0200

    Wrote most of the consolidate code.

--- results/consolidate.m
+++ results/consolidate.m
@@ -1,8 +1,23 @@
-function [] = consolidate( prefix )
+function [] = consolidate( regular_expression )
 %CONSILIDATE Consolidate the files of a dataset into one file.
 
-% Find all 2D files
-% Check for doubles
+    files = findFiles(regular_expression);
+    experiments2.parameters = [];
+    experiments2.statistics = [];
+    
+    exp_idx = 1;
+    for idx = 1:numel(files)
+       new_data = load(files{idx}); 
+       statistics = [new_data.experiments.statistics];
+       parameters = [new_data.experiments.parameters];
+       for new_data_idx = 1:numel(statistics)
+           experiments2(exp_idx).statistics = statistics(new_data_idx);
+           experiments2(exp_idx).parameters = parameters(new_data_idx);
+       end
+    end
+   
+    sprintf('hi!')
+        
 % Create empty experiments struct
 % For each of the 2D files
     % Read the contents
@@ -12,7 +27,17 @@ function [] = consolidate( prefix )
     % ELSE Write to 2D.mat
 end
 
-function [files] = findFiles(prefix)
-    possible_files = dir(sprintf('%s*.mat', prefix));
-    
+function [files] = findFiles(regular_expression)
+    possible_files = dir();
+    possible_files = {possible_files.name}';
+    files = {};
+    file_idx = 1;
+    for i = 1:numel(possible_files)
+        file_name = possible_files{i};
+        result = regexp(file_name, regular_expression, 'match');
+        if ~ isempty(result)
+            files{file_idx} = result{1};
+            file_idx = file_idx + 1;
+        end
+    end
 end
--- results/consolidateResults.m
+++ results/consolidateResults.m
@@ -1,3 +1,4 @@
 close all; clear variables; clc;
 
-consolidate('2D');
+consolidate('2D_\d+_\d+\.mat');
+
