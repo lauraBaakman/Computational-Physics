d527bdbc1f45c10a794e21dfae2394a24497f02f
Laura Baakman <laurabaakman@gmail.com>
2016-10-07 16:02:55 +0200

    Fixed naming

--- code/+output/generateLaTeXTable.m
+++ code/+output/generateLaTeXTable.m
@@ -1,10 +1,10 @@
-function [mean_accuracy] = generateLaTeXTable( experiments, numSampleIterations, numParticles)
+function [mean_accuracy] = generateLaTeXTable( experiments, numSampleIterations, numParticles, numIterations)
 %GENERATELATEXTABLE Generate a LaTeX table for the experiments.
     data = collectData(experiments, numSampleIterations, numParticles);
     
     mean_accuracy = meanAccuracy(data);
     
-    outputFile = generatePath(numSampleIterations, numParticles);
+    outputFile = generatePath(numIterations, numParticles);
     
     fileID = fopen(outputFile,'w+');
     fprintf(fileID, '%%!TEX root = ../report.tex\n\n');
@@ -13,9 +13,9 @@ function [mean_accuracy] = generateLaTeXTable( experiments, numSampleIterations,
     
 end
 
-function [outputFilePath] = generatePath(numSampleIterations, numParticles)
+function [outputFilePath] = generatePath(numIterations, numParticles)
     outputFilePath = sprintf('../report/tables/NS%d_N%d.tex', ...
-        numSampleIterations, numParticles);
+        numIterations, numParticles);
 end
 
 function [average] = meanAccuracy(data)
