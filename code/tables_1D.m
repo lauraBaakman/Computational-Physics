8804f4b6d4e665232904bad79a7eafba60ae68de
Laura Baakman <laurabaakman@gmail.com>
2016-10-07 15:01:28 +0200

    Keep track of an index and take the difference between numSampleIterations and numIterations into account.

--- code/tables_1D.m
+++ code/tables_1D.m
@@ -2,12 +2,19 @@ clc; close all; clear all;
 load('../results/1D.mat');
 
 numParticlesList = [10, 100, 1000];
-numSampleIterationsList = [1000, 10000];
+numIterationsList = [1000, 10000];
 
-   for numParticles = numParticlesList
-      for numSampleIterations =  numSampleIterationsList
+accuracy = nan(length(numIterationsList), length(numParticlesList));
+
+   for numParticles_idx = 1:length(numParticlesList)
+      numParticles = numParticlesList(numParticles_idx);
+      for numIterations_idx =  1:length(numIterationsList)
+        numIterations = numIterationsList(numIterations_idx);
+        
+        numSampleIterations = numIterations * numParticles;
         accuracy = output.generateLaTeXTable(experiments, numSampleIterations, numParticles);          
         fprintf('%d %d %f\n', numParticles, numSampleIterations, accuracy)
+        
       end
       fprintf('\n')
    end
